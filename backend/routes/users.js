0d05f3a8619236a413f92d17072977aea2a6cdc8
anthonyyyyyyy <anthony.selak@edu.ece.fr>
2019-12-10 23:21:31 +0100

    routes

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../controllers/users-controllers.js');
+var User = require('../models/users-models.js');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// GET all users
+router.get('/:getAllusers', User.findAll);
+
+// GET one user
+router.get('/:getOneuser', User.findOne);
+
+router.get("/getUser", User.findOne);
+
+// PUT Create one user
+router.put('/', User.create);
+
+// POST Update one user
+router.post('/:userId', User.update);
+
+// DELETE Delete one user
+router.delete('/:userId', User.delete);
 
 module.exports = router;
